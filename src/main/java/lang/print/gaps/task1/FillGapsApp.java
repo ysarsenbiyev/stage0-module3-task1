d85bad3d900d103b4df2a694be1f98199c9d8fda
ysarsenbiyev <sarsenbiev01@gmail.com>
2022-07-29 22:03:00 +0600

    stage0-module3-task1

--- src/main/java/lang/print/gaps/task1/FillGapsApp.java
+++ src/main/java/lang/print/gaps/task1/FillGapsApp.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task1;
 
 public class FillGapsApp {
     public static void main(String[] args) {
-          System.out.println("\nFixed");
+          System.out.println("Fixed");
     }
 }
