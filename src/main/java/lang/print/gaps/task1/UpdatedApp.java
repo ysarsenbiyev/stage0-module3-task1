11aa3cb46668ba376d49b6a53555892f806d91fa
ysarsenbiyev <sarsenbiev01@gmail.com>
2022-08-02 11:09:58 +0600

    stage0-module3-task1

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task1;
 
 public class UpdatedApp {
     public static void main(String[] args) {
-        System.out.println("I'm\nupdated" + "\n"+"can\nbe\ncompiled");
+        System.out.println("I'm\nupdated\ncan\nbe\ncompiled.\nAnd\ncan\nwrite\nto\nconsole");
     }
 }
